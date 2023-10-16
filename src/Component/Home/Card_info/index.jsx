fd158f8a1824766f6f05148dcfeab0a7ec29bf36
Jonathan0008 <s22110362@student.unklab.ac.id>
2023-10-16 21:36:35 +0700

    Update commit

---
+++ src/Component/Home/Card_info/index.jsx
@@ -0,0 +1,15 @@
+import Logo from "../../Header/Logo";
+import Type_Effect from "./Type_Effect";
+import Evolution from "./Evolution";
+
+const Card_info = () => {
+  return (
+    <>
+      <Logo />
+      <Type_Effect />
+      <Evolution />
+    </>
+  );
+  };
+  
+  export default Card_info;
